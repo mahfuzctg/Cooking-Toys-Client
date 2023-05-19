51678f16a8337ab06b44abc12d96279e7225bc0a
Mahfuz <aamahfuz.pro@gmail.com>
2023-05-19 22:37:34 +0600

    added firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTe-l1bWh8qnr5-FIhhSe7quTRaEjDsZM",
-  authDomain: "cooking-toys-72aa8.firebaseapp.com",
-  projectId: "cooking-toys-72aa8",
-  storageBucket: "cooking-toys-72aa8.appspot.com",
-  messagingSenderId: "960361941189",
-  appId: "1:960361941189:web:417e04fc80601fdac788ac",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
